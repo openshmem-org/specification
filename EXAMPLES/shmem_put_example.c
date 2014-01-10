c1d0fab22fc692057e62df02ea024742a3ecc26a
Swaroop Pophale <Swaroop@Swaroops-MacBook-Pro.local>
2014-01-10 17:25:39 -0500

    Adding missing example.

---
+++ EXAMPLES/shmem_put_example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <shmem.h>
+main()
+{
+   long source[10] = { 1, 2, 3, 4, 5,
+   		    6, 7, 8, 9, 10 };
+   static long target[10];
+   start_pes(0);
+   if (_my_pe() == 0) {
+      /* put 10 words into target on PE 1 */
+      shmem_long_put(target, source, 10, 1);
+   }
+   shmem_barrier_all();  /* sync sender and receiver */
+   printf("target[0] on PE %d is %d\n", _my_pe(), target[0]);
+}
+
