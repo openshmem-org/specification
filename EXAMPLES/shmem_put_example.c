1bb06ec7231c81016e6166dd48becba898d60316
Mitchell, Eric W <mitchellew@ornl.gov>
2014-02-05 15:00:24 -0500

    shmem_put_example.c list alignment fix

--- EXAMPLES/shmem_put_example.c
+++ EXAMPLES/shmem_put_example.c
@@ -3,7 +3,7 @@
 main()
 {
    long source[10] = { 1, 2, 3, 4, 5,
-   		    6, 7, 8, 9, 10 };
+   		                 6, 7, 8, 9, 10 };
    static long target[10];
    start_pes(0);
    if (_my_pe() == 0) {
