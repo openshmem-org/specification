f300b8e2dfb341ad4cfa90bc566328f3ef840761
Mitchell, Eric W <mitchellew@ornl.gov>
2014-02-05 11:08:17 -0500

    Examples using the same margins examples_C.tex all2all_subs.c, pi.c, and hello.f90

--- C/all2all_subs.c
+++ C/all2all_subs.c
@@ -17,40 +17,40 @@ int64  do_all2all      (uint64 *dst, uint64 *src, int64 len, int64 nwrd)
 {
 static char cvs_info[] = "BMKGRP $Date: $ $Revision: $ $RCSfile: all2all.c,v $ $Name: $";
 
-  int64 i, j, pe;
+   int64 i, j, pe;
 
-  len = len - (len % (nwrd * SIZE));  // force even multiple
-  for (i = 0; i < len; i+=SIZE*nwrd) {
-    shmem_barrier_all();
-    for (j = 0; j < SIZE; j++) {
-      pe = PERM(SELF,SIZE,j);
-      /* shmem_put (&dst[i + SELF*nwrd], &src[i + pe*nwrd], nwrd, pe);*/
-      shmem_put64 (&dst[i + SELF*nwrd], &src[i + pe*nwrd], nwrd, pe);
-    }
-  }
-  return len;
+   len = len - (len % (nwrd * SIZE));  // force even multiple
+   for (i = 0; i < len; i+=SIZE*nwrd) {
+      shmem_barrier_all();
+      for (j = 0; j < SIZE; j++) {
+         pe = PERM(SELF,SIZE,j);
+         /* shmem_put (&dst[i + SELF*nwrd], &src[i + pe*nwrd], nwrd, pe);*/
+         shmem_put64 (&dst[i + SELF*nwrd], &src[i + pe*nwrd], nwrd, pe);
+      }
+   }
+   return len;
 }
 
 int64  accum_long      (int64 val)
 
 {
-  int64 i;
-  static int64 target, source, init=0;
-  static int64 Sync[_SHMEM_REDUCE_SYNC_SIZE];
-  static int64 Work[2 + _SHMEM_REDUCE_MIN_WRKDATA_SIZE];
+   int64 i;
+   static int64 target, source, init=0;
+   static int64 Sync[_SHMEM_REDUCE_SYNC_SIZE];
+   static int64 Work[2 + _SHMEM_REDUCE_MIN_WRKDATA_SIZE];
 
-  if (! init) {
-    /* need to initialize Sync first time around */
-    for(i = 0; i < _SHMEM_REDUCE_SYNC_SIZE; i++)
-      Sync[i] = _SHMEM_SYNC_VALUE;
-    init = 1;
-  }
-  source = val;
-  shmem_barrier_all();
+   if (! init) {
+      /* need to initialize Sync first time around */
+      for(i = 0; i < _SHMEM_REDUCE_SYNC_SIZE; i++)
+         Sync[i] = _SHMEM_SYNC_VALUE;
+      init = 1;
+   }
+   source = val;
+   shmem_barrier_all();
 
-  shmem_long_sum_to_all (&target, &source, 1, 0, 0, SIZE, Work, Sync);
+   shmem_long_sum_to_all (&target, &source, 1, 0, 0, SIZE, Work, Sync);
 
-  shmem_barrier_all();
-  return target;
+   shmem_barrier_all();
+   return target;
 }
 
--- C/pi.c
+++ C/pi.c
@@ -35,71 +35,71 @@ int main(int argc, char *argv[])
 // 3. hit X < sin(theta)
 // 4. 2/pi = hit/tries.
 
-  double X, Theta, My_pi;
-  double Tstart, Tend;
-  int i, total, hit=0;
-  int *buf, my_mem;
-  int rank, numprocs, num_of_procs;
+   double X, Theta, My_pi;
+   double Tstart, Tend;
+   int i, total, hit=0;
+   int *buf, my_mem;
+   int rank, numprocs, num_of_procs;
  
-  num_of_procs = atoi(argv[1]);
+   num_of_procs = atoi(argv[1]);
 
-  start_pes (0);
+   start_pes (0);
 
-  numprocs = _num_pes();
-  rank     = _my_pe();
+   numprocs = _num_pes();
+   rank     = _my_pe();
 
-  my_mem = (sizeof(int) * numprocs);
-  buf = shmalloc(my_mem);
-
-  srand( (unsigned int) time(NULL));
-
-  if ( rank == 0 ) {
-    printf("pi is %f\n", M_PI_2 );
-    printf("sin(pi/2) is %f\n", sin(M_PI_2));
-    fflush(stdout);
-  }
-
-  Tstart = timerval();
-
-  if ( rank != 0 ) {
-   total = TRIES/(numprocs-1);
-   if (rank == 1)
-     total += TRIES % (numprocs-1);
+   my_mem = (sizeof(int) * numprocs);
+   buf = shmalloc(my_mem);
 
    srand( (unsigned int) time(NULL));
 
-   for ( i = 0; i < total ; i++){
-   X = rand();
-   X = X/RAND_MAX;
-
-   Theta = rand();
-   Theta = ( M_PI_2 ) * (Theta/RAND_MAX);
-   if ( X < sin(Theta))
-     hit++;
+   if ( rank == 0 ) {
+      printf("pi is %f\n", M_PI_2 );
+      printf("sin(pi/2) is %f\n", sin(M_PI_2));
+      fflush(stdout);
    }
-   buf[0] = hit;
-  }
 
-  shmem_barrier_all();
+   Tstart = timerval();
 
-  if ( rank == 0 )
-    for ( i = 1; i < numprocs; i++) {
-      shmem_getmem(buf, buf, sizeof(int), i);
-      hit += buf[0];
-      printf("from node(%d), getmem buf\t %d, so hit is\t %d\n", i, buf[0], hit);
+   if ( rank != 0 ) {
+      total = TRIES/(numprocs-1);
+         if (rank == 1)
+            total += TRIES % (numprocs-1);
+
+    srand( (unsigned int) time(NULL));
+
+    for ( i = 0; i < total ; i++){
+    X = rand();
+    X = X/RAND_MAX;
+
+    Theta = rand();
+    Theta = ( M_PI_2 ) * (Theta/RAND_MAX);
+    if ( X < sin(Theta))
+       hit++;
     }
+   buf[0] = hit;
+   }
 
-  shmem_barrier_all();
+   shmem_barrier_all();
 
-  if ( rank == 0 ) {
-  My_pi = 2 * ( 1 / ( ((double)hit)/TRIES ) );
-  Tend = timerval();
-  printf("Hit is : %d :: Total is %d \n",hit, TRIES);
-  printf("My pi is %.16f \n", My_pi);
-  printf("Elapsed time is %f \n", Tend - Tstart);
-  }
+   if ( rank == 0 )
+      for ( i = 1; i < numprocs; i++) {
+         shmem_getmem(buf, buf, sizeof(int), i);
+         hit += buf[0];
+         printf("from node(%d), getmem buf\t %d, so hit is\t %d\n", i, buf[0], hit);
+      }
 
-  shmem_barrier_all();
+   shmem_barrier_all();
 
-  return 0;
+   if ( rank == 0 ) {
+   My_pi = 2 * ( 1 / ( ((double)hit)/TRIES ) );
+   Tend = timerval();
+   printf("Hit is : %d :: Total is %d \n",hit, TRIES);
+   printf("My pi is %.16f \n", My_pi);
+   printf("Elapsed time is %f \n", Tend - Tstart);
+   }
+
+   shmem_barrier_all();
+
+   return 0;
 }
