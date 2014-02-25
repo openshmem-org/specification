1e777c74522a7eec4e7a1da27b4d293ad1065c4a
Swaroop Pophale <swaroop.pophale23@gmail.com>
2014-02-24 20:25:25 -0600

    Tested with reference implementation.

---
+++ EXAMPLES/shmem_quiet_example.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <shmem.h>
+
+long target[3] = {0};
+int targ = 0;
+long source[3] = {1, 2, 3};
+int src = 90;
+
+int main(void)
+{
+  start_pes(0);
+  if (_my_pe() == 0) {
+    shmem_long_put(target, source, 3, 1);  /*put1*/
+    shmem_int_put(&targ, &src, 1, 2);  /*put4*/
+
+    shmem_quiet();
+
+    shmem_long_get(target, source, 3, 1);
+    shmem_int_get(&targ, &src, 1, 2);      
+    printf("target: {%d,%d,%d}\n",target[0],target[1],target[2]); /*target: {1,2,3}*/
+    printf("targ: %d\n", targ); /*targ: 90*/
+
+    shmem_int_put(&targ, &src, 1, 1);  /*put3*/
+    shmem_int_put(&targ, &src, 1, 2);  /*put4*/
+  }
+  shmem_barrier_all();  /* sync sender and receiver */
+  printf("target[0] on PE %d is %d\n", _my_pe(), target[0]);
+  return 0;
+}
+
