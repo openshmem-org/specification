3529a5ae9beb15bd3acbd887aa658de3842f0744
Graham Lopez <lopezmg@ornl.gov>
2015-02-09 19:41:48 -0500

    started converting to cleaner latex

---
+++ new_latex/example_code/shmem_finalize_example.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <shmem.h> 
+
+long x = 10101;
+
+int main(void) {
+   int me, npes; 
+   long y = -1;
+
+   shmem_init();
+
+   me = shmem_my_pe();
+   npes = shmem_n_pes();
+   if (me == 0)
+     y = shmem_long_g(&x, npes-1);
+
+   printf("%d: y = %ld\n", me, y); 
+
+   shmem_finalize();  
+   return 0;
+}
---
+++ new_latex/example_code/shmem_ptr_example.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <shmem.h>
+
+int main(void) 
+{
+   static int bigd[100];
+      int *ptr;
+      int i;
+
+   shmem_init();
+
+   if (shmem_my_pe() == 0) {
+      /* initialize PE 1's bigd array */
+      ptr = shmem_ptr(bigd, 1);
+      if (ptr == NULL)
+         printf("can't use pointer to directly access PE 1's array\n");
+      else
+         for (i=0; i<100; i++)
+            *ptr++ = i+1;
+   }
+
+   shmem_barrier_all();
+
+   if (shmem_my_pe() == 1) {
+      printf("bigd on PE 1 is:\n");
+      for (i=0; i<100; i++)
+         printf(" %d\n",bigd[i]);
+      printf("\n");
+   }
+  return 1;
+}
