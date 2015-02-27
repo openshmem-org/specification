c3f36629591aa496c38ca67f422eae22e3087270
Tony Curtis <tonyc@uh.edu>
2015-02-27 09:48:17 -0600

    global_exit example didn't work, have fixed it (and tested too)

--- EXAMPLES/shmem_global_exit_example.c
+++ EXAMPLES/shmem_global_exit_example.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <shmem.h> 
 
-int main(void) {
+int
+main(void)
+{
   int me, npes; 
 
-   shmem_init();
+  shmem_init();
 
-   me = shmem_my_pe();
-   npes = shmem_n_pes();
-   if (me == 0){
-     char ch;
-     FILE *fp;
-     int status;
+  me = shmem_my_pe();
+  npes = shmem_n_pes();
 
-     fp = fopen(input.txt,"r"); 
-     if( fp == NULL ){          /*Input file required by program is not available*/
-       status = EXIT_FAILURE;
-       shmem_global_exit(status);
-     }
-   }
+  if (me == 0) {
+    FILE *fp = fopen("input.txt", "r"); 
 
-   fclose(fp);
+    if (fp == NULL) {  /* Input file required by program is not available */
+      shmem_global_exit(EXIT_FAILURE);
+    }
+
+    /* do something with the file */
+
+    fclose(fp);
+  }
 
   return 0;
 }
-
