edfe0815a0623931f97a538264a09150dcf90214
Graham Lopez <lopezmg@ornl.gov>
2015-07-06 13:55:55 -0400

    added first draft of shmem_alltoall/v

---
+++ example_code/shmem_alltoall_example.c
@@ -0,0 +1,48 @@
+#include <shmem.h>
+#include <stdio.h>
+main()
+{
+    static long pSync[_SHMEM_ALLTOALL_SYNC_SIZE];
+    long *source, *dest;
+    int  i, count, pe;
+    
+    start_pes(0);
+    
+    count = 2;
+    dest = (long*) shmalloc(count * shmem_n_pes() * sizeof(long));
+    source = (long*) shmalloc(count * shmem_n_pes() * sizeof(long));
+    
+    /* assign source values */
+    for (pe=0; pe <shmem_n_pes(); pe++){
+       for (i=0; i<count; i++){
+          source[*pe*count)+i] = shmem_my_pe() + pe;
+          dest[(pe*count)+i] = 9999;
+       }
+    }
+    
+    for (i=0; i<_SHMEM_ALLTOALL_SYNC_SIZE; i++) {
+       pSync[i] = _SHMEM_SYNC_VALUE;
+    }
+    /* wait for all PEs to initialize pSync */
+    shmem_barrier_all();
+    
+    /* alltoall on all PES */
+    shmem_alltoall(dest, source, count * sizeof(long), 0, 0,
+                    shmem_n_pes(), pSync);
+    
+    /* verify results */
+    for (pe=0; pe<shmem_n_pes(); pe++) {
+       for (i=0; i<count; i++){
+          if (dest[(pe*count)+i] != shmem_my_pe() + pe) {
+          printf("[%d] ERROR: dest[%d]=%ld, should be %d\n",
+                  shmem_my_pe(),(pe*count)+i,dest[(pe*count)+i],
+                  shmem_n_pes() + pe);
+            }
+        }
+    }
+    
+    shmem_barrier_all();
+    shfree(dest);
+    shfree(source);
+    shmem_finalize();
+}
