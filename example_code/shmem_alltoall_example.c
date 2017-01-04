a2f6f867558105ec1f87e3d86598e265e5fef2e8
James A. Ross <james.a.ross176.civ@mail.mil>
2017-01-04 19:23:48 +0000

    Bug fix in printf output, removed barrier
    There was a bug in the printf output.  The value should have be 'pe+me' not
    'npes+pe'.  An unnecessary explicit barrier was also removed. The formatting of
    the code was made consistent with other (4 to 3 spaces for a tab).  The global
    sync array was made to static and a barrier is only needed to for
    initialization of source and destination arrays.  The sync array is now
    initialized before the implicit barier in shmem_init().

--- example_code/shmem_alltoall_example.c
+++ example_code/shmem_alltoall_example.c
@@ -2,50 +2,45 @@
 #include <inttypes.h>
 #include <shmem.h>
 
-long pSync[SHMEM_ALLTOALL_SYNC_SIZE];
-
 int main(void)
 {
-    int64_t *source, *dest;
-    int  i, count, pe;
-    
-    shmem_init();
-    
-    count = 2;
-    dest = (int64_t*) shmem_malloc(count * shmem_n_pes() * sizeof(int64_t));
-    source = (int64_t*) shmem_malloc(count * shmem_n_pes() * sizeof(int64_t));
-    
-    /* assign source values */
-    for (pe=0; pe <shmem_n_pes(); pe++){
-       for (i=0; i<count; i++){
-          source[(pe*count)+i] = shmem_my_pe() + pe;
-          dest[(pe*count)+i] = 9999;
+   static long pSync[SHMEM_ALLTOALL_SYNC_SIZE];
+   for (int i = 0; i < SHMEM_ALLTOALL_SYNC_SIZE; i++)
+      pSync[i] = SHMEM_SYNC_VALUE;
+
+   shmem_init();
+   int me = shmem_my_pe();
+   int npes = shmem_n_pes();
+
+   const int count = 2;
+   int64_t* dest = (int64_t*) shmem_malloc(count * npes * sizeof(int64_t));
+   int64_t* source = (int64_t*) shmem_malloc(count * npes * sizeof(int64_t));
+
+   /* assign source values */
+   for (int pe = 0; pe < npes; pe++) {
+      for (int i = 0; i < count; i++) {
+         source[(pe * count) + i] = me + pe;
+         dest[(pe * count) + i] = 9999;
+      }
+   }
+   /* wait for all PEs to update source/dest */
+   shmem_barrier_all();
+
+   /* alltoall on all PES */
+   shmem_alltoall64(dest, source, count, 0, 0, npes, pSync);
+
+   /* verify results */
+   for (int pe = 0; pe < npes; pe++) {
+      for (int i = 0; i < count; i++) {
+         if (dest[(pe * count) + i] != pe + me) {
+            printf("[%d] ERROR: dest[%d]=%" PRId64 ", should be %d\n",
+               me, (pe * count) + i, dest[(pe * count) + i], pe + me);
+           }
        }
-    }
-    
-    for (i=0; i< SHMEM_ALLTOALL_SYNC_SIZE; i++) {
-       pSync[i] = SHMEM_SYNC_VALUE;
-    }
-    /* wait for all PEs to initialize pSync */
-    shmem_barrier_all();
-    
-    /* alltoall on all PES */
-    shmem_alltoall64(dest, source, count, 0, 0, shmem_n_pes(), pSync);
-    
-    /* verify results */
-    for (pe=0; pe<shmem_n_pes(); pe++) {
-       for (i=0; i<count; i++){
-          if (dest[(pe*count)+i] != shmem_my_pe() + pe) {
-          printf("[%d] ERROR: dest[%d]=%" PRId64 ", should be %d\n",
-                  shmem_my_pe(),(pe*count)+i,dest[(pe*count)+i],
-                  shmem_n_pes() + pe);
-            }
-        }
-    }
-    
-    shmem_barrier_all();
-    shmem_free(dest);
-    shmem_free(source);
-    shmem_finalize();
-    return 0;
+   }
+
+   shmem_free(dest);
+   shmem_free(source);
+   shmem_finalize();
+   return 0;
 }
