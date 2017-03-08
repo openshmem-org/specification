c93f15d6cd397ab2c0d51c4c6df8f6ff78a97e44
James A. Ross <james.a.ross176.civ@mail.mil>
2017-03-08 21:36:28 +0000

    combining mype and n_pes examples

--- example_code/shmem_npes_example.c
+++ example_code/shmem_npes_example.c
@@ -6,8 +6,7 @@ int main(void)
    shmem_init();
    int me = shmem_my_pe();
    int npes = shmem_n_pes();
-   if (me == 0)
-      printf("Number of PEs executing this program is: %d\n", npes);
+   printf("I am #%d of %d PEs executing this program\n", me, npes);
    shmem_finalize();
    return 0;
 }
