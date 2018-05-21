3497fc060aacd9b3566c7cb6c304130fa320cb99
David M. Ozog <david.m.ozog@intel.com>
2018-05-21 11:56:26 -0400

    Clean up the shmem_wait_until_some example code
    Signed-off-by: David M. Ozog <david.m.ozog@intel.com>

--- example_code/shmem_wait_until_some_all2all_sum.c
+++ example_code/shmem_wait_until_some_all2all_sum.c
@@ -9,7 +9,6 @@
 int main(void)
 {
   int total_sum = 0;
-  size_t count = 0;
 
   shmem_init();
   int mype = shmem_my_pe();
@@ -18,35 +17,33 @@ int main(void)
   int my_data = malloc(N * sizeof(int));
   int *all_data = shmem_malloc(N * npes * sizeof(int));
   int *flags = shmem_calloc(npes, sizeof(int));
-
-  _Bool *processed = calloc(npes, sizeof(_Bool));
   _Bool *completed = calloc(npes, sizeof(_Bool));
+  _Bool *processed = calloc(npes, sizeof(_Bool));
 
-  for (int i=0; i<N; i++) {
+  for (int i = 0; i < N; i++)
     my_data[i] = mype*N + i;
-  }
 
-  for (int i=0; i<npes; i++)
+  for (int i = 0; i < npes; i++)
     shmem_put_nbi(&all_data[i*N], my_data, N, i);
 
   shmem_fence();
 
-  for (int i=0; i<npes; i++)
+  for (int i = 0; i < npes; i++)
     shmem_p(&flags[i], 1, i);
   
-  while (count = shmem_wait_until_some(flags, npes, completed, SHMEM_CMP_NE, 0)) {
+  while (shmem_wait_until_some(flags, npes, completed, SHMEM_CMP_NE, 0)) {
 
-    for (int i=0; i<npes; i++) {
+    for (int i = 0; i < npes; i++) {
       if (completed[i] == 1 && !processed[i] == 1) {
-        for (int j=0; j<N; j++)
+        for (int j = 0; j < N; j++)
           total_sum += all_data[i*N+ j];
         processed[i] = 1;
       }
     }
   }
 
-  int M = N * (npes - 1);
-  assert(total_sum == ((M + N - 1) * (M + N)) / 2);
+  int M = N * npes - 1;
+  assert(total_sum == M * (M + 1) / 2);
 
   free(my_data);
   free(completed);
