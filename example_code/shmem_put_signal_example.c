47bc318046c3c543e64f076e3b664c105dab29cd
Naveen Namashivayam Ravichandrasekaran <nravi.research@gmail.com>
2018-10-08 17:02:33 -0500

    Fix review comments for PWS example
    1. Use calloc and avoid barriers from malloc and explicit calls
    2. use all C11 generic shmem calls
    3. Follow shmem bcast semantics - to bcast to source itself
    4. convert wavefront-like transfer semantics to true bcast

--- example_code/shmem_put_signal_example.c
+++ example_code/shmem_put_signal_example.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
 #include <shmem.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    int i, err_count  = 0;
+    int i, err_count = 0;
 
     shmem_init();
 
-    size_t    size    = (2<<10);
-    int       me      = shmem_my_pe();
-    int       n       = shmem_n_pes();
-    int       pe      = (me + 1)%n;
-    uint64_t* message = malloc(size * sizeof(uint64_t));
-    uint64_t* data    = shmem_malloc(size * sizeof(uint64_t));
+    size_t          size     = 2048;
+    int             me       = shmem_my_pe();
+    int             n        = shmem_n_pes();
+    int             pe       = (me + 1) % n;
+    uint64_t *      message  = malloc(size * sizeof(uint64_t));
     static uint64_t sig_addr = 0;
 
     for (i = 0; i < size; i++) {
         message[i] = me;
-        data[i]    = 0;
     }
-    shmem_barrier_all();
+
+    uint64_t *data = shmem_calloc(size, sizeof(uint64_t));
 
     if (me != 0) {
-        shmem_uint64_wait_until(&sig_addr, SHMEM_CMP_EQ, 1);
+        shmem_wait_until(&sig_addr, SHMEM_CMP_EQ, 1);
+        memcpy(message, data, size * sizeof(uint64_t));
     }
 
-    shmem_putmem_signal(data, message, size*sizeof(uint64_t),
-            &sig_addr, 1, pe);
-
-    if (me == 0) {
-        shmem_uint64_wait_until(&sig_addr, SHMEM_CMP_EQ, 1);
-        printf("BCAST with put with signal is complete\n");
+    if (me != (n - 1)) {
+        shmem_put_signal(data, message, size, &sig_addr, 1, pe);
     }
 
     free(message);
