c03568aa17dd17977794b347287fac127ae42dd6
Md <md.rahman@intel.com>
2018-10-30 23:44:06 -0500

    Added limitations section; incorporated Jim's feedback
    Signed-off-by: Md <md.rahman@intel.com>

--- example_code/pshmem_example.c
+++ example_code/pshmem_example.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#include <shmemx.h>
+#include <sys/time.h>
 #include <pshmem.h>
 
-static double totalTimeForPut = 0.0;
-static double avgTimeForPut = 0.0;
-static long countForPut = 0;
+static double total_put_time = 0.0;
+static double avg_put_time = 0.0;
+static long put_count = 0;
+
+static inline double get_wtime(void) {
+    double wtime = 0.0;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    wtime = tv.tv_sec;
+    wtime += (double)tv.tv_usec / 1.0e6;
+    return wtime;
+}
 
 void shmem_long_put(long *target, const long *source, size_t nelems, int pe)
 {
-    double tstart = shmemx_wtime();                                           /* Start timer */
+    double t_start = get_wtime();                                             /* Start timer */
     pshmem_long_put(target, source, nelems, pe);                 /* Name shifted call to put */
-    totalTimeForPut += shmemx_wtime() - tstart;              /* Calculate total time elapsed */
-    countForPut += 1;                                                /* Increment put counts */
-    avgTimeForPut = totalTimeForPut / (double) countForPut; /* Calculate average put latency */
+    total_put_time += get_wtime() - t_start;                 /* Calculate total time elapsed */
+    put_count += 1;                                                  /* Increment put counts */
+    avg_put_time = total_put_time / (double) put_count;     /* Calculate average put latency */
 
     return;
 }
