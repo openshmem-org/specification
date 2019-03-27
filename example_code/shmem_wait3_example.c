206b23dcb18f3a258b83905dbba5036820ec8a68
James Dinan <james.dinan@intel.com>
2019-03-26 20:49:04 -0400

    Update wait example to be valid C code
    Signed-off-by: James Dinan <james.dinan@intel.com>

--- example_code/shmem_wait3_example.c
+++ example_code/shmem_wait3_example.c
@@ -1,4 +1,8 @@
-#include <stdio.h>#include <shmem.h>
+#include <stdio.h>
+#include <shmem.h>
 
 int ivar;
-shmem_int_wait_until(&ivar, SHMEM_CMP_LT, 0);
+
+void wait_on_ivar(void) {
+    shmem_int_wait_until(&ivar, SHMEM_CMP_LT, 0);
+}
