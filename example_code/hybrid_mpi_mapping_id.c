7723dfa0b262034d52c77d633dde6ab5d27e67a3
Min Si <msi@anl.gov>
2019-10-28 17:20:59 -0500

    interop: adjust to use teams API

--- example_code/hybrid_mpi_mapping_id.c
+++ example_code/hybrid_mpi_mapping_id.c
@@ -4,23 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-    static long pSync[SHMEM_COLLECT_SYNC_SIZE];
-    for (int i = 0; i < SHMEM_COLLECT_SYNC_SIZE; i++)
-        pSync[i] = SHMEM_SYNC_VALUE;
-
     MPI_Init(&argc, &argv);
     shmem_init();
 
-    int mype = shmem_my_pe();
-    int npes = shmem_n_pes();
+    int mype = shmem_team_my_pe(SHMEM_TEAM_WORLD);
+    int npes = shmem_team_n_pes(SHMEM_TEAM_WORLD);
 
     static int myrank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
     int *mpi_ranks = shmem_calloc(npes, sizeof(int));
 
-    shmem_collect32(mpi_ranks, &myrank, 1, 0, 0, npes, pSync);
-
+    shmem_int_collect(SHMEM_TEAM_WORLD, mpi_ranks, &myrank, 1);
     if (mype == 0)
         for (int i = 0; i < npes; i++)
             printf("PE %d's MPI rank is %d\n", i, mpi_ranks[i]);
