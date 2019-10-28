192710f0804e13e5ea89592725f48ad6cb8c888c
Min Si <msi@anl.gov>
2019-10-28 11:55:37 -0500

    interop/query: header fix and use larger data size in example

--- example_code/shmem_query_mpi_progress.c
+++ example_code/shmem_query_mpi_progress.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <shmem.h>
 #include <mpi.h>
+
+int a[1048576];
 
 int main(int argc, char *argv[])
 {
@@ -12,17 +14,16 @@ int main(int argc, char *argv[])
     if (!shmem_query_interoperability(SHMEM_PROGRESS_MPI))
         shmem_global_exit(EXIT_FAILURE);
 
-    int a[100];
     static int b = 0;
     if (mype == 0) {
         MPI_Request req = MPI_REQUEST_NULL;
-        MPI_Isend(a, 100, MPI_INT, 1, 0, MPI_COMM_WORLD, &req);
+        MPI_Isend(a, 1048576, MPI_INT, 1, 0, MPI_COMM_WORLD, &req);
 
         while (shmem_int_atomic_fetch(&b, 0) != 1);
 
-        MPI_Wait(req, MPI_STATUS_IGNORE);
+        MPI_Wait(&req, MPI_STATUS_IGNORE);
     } else {
-        MPI_Recv(a, 100, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(a, 1048576, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         shmem_int_atomic_set(&b, 1, 0);
     }
