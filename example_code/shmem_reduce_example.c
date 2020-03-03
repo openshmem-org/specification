f16e72be85bd3e9907bfd4f6df3c63713ba32e87
David M. Ozog <david.m.ozog@intel.com>
2020-03-03 16:41:30 -0500

    Update reduction example text, add an OR reduction
    Signed-off-by: David M. Ozog <david.m.ozog@intel.com>

--- example_code/shmem_reduce_example.c
+++ example_code/shmem_reduce_example.c
@@ -11,25 +11,37 @@ int main(void) {
 
   int *values = shmem_malloc(NELEMS * sizeof(int));
 
-  static int my_maximal_values = 0;
-  static int all_maximal_values;
+  unsigned char *value_is_maximal     = shmem_malloc(NELEMS * sizeof(unsigned char));
+  unsigned char *value_is_maximal_all = shmem_malloc(NELEMS * sizeof(unsigned char));
+
+  static int maximal_values_count = 0;
+  static int maximal_values_total;
 
   srand((unsigned)mype);
 
-  for (int i = 0; i < NELEMS ; i++) {
+  for (int i = 0; i < NELEMS; i++) {
     values[i] = rand() % npes;
 
-    /* Count the instances of maximal values (i.e., equal to (npes-1)) */
-    my_maximal_values += (values[i] == (npes - 1)) ? 1 : 0;
+    /* Track and count instances of maximal values (i.e., values equal to (npes-1)) */
+    value_is_maximal[i] = (values[i] == (npes - 1)) ? 1 : 0;
+    maximal_values_count += value_is_maximal[i];
   }
 
   /* Wait for all PEs to initialize reductions arrays */
   shmem_sync(SHMEM_TEAM_WORLD);
 
-  shmem_sum_reduce(SHMEM_TEAM_WORLD, &all_maximal_values, &my_maximal_values, 1);
+  shmem_or_reduce(SHMEM_TEAM_WORLD, value_is_maximal_all, value_is_maximal, NELEMS);
+  shmem_sum_reduce(SHMEM_TEAM_WORLD, &maximal_values_total, &maximal_values_count, 1);
 
-  if (mype == 0)
-    printf("found a total of %d maximal random numbers\n", all_maximal_values);
+  if (mype == 0) {
+    printf("Found %d maximal random numbers across all PEs.\n", maximal_values_total);
+    printf("A maximal number occured (at least once) at the following indices:\n");
+    for (int i = 0; i < NELEMS; i++) {
+      if (value_is_maximal_all[i] == 1) {
+        printf("%d ", i);
+      }
+    } printf("\n");
+  }
 
   shmem_finalize();
   return 0;
