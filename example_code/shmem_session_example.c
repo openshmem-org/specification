9d7b266f778ea4c9a8ffad4299656d59de157c73
David Ozog <david.m.ozog@intel.com>
2022-09-23 00:08:05 -0600

    Sessions: rewrite bundles text to suit sessions

--- example_code/shmem_session_example.c
+++ example_code/shmem_session_example.c
@@ -1,27 +1,31 @@
 #include <shmem.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-#define N_UPDATES (1 << 20)
-#define N_INDICES (1 << 10)
-#define N_VALUES  (1 << 31)
+#define N_UPDATES (1lu << 22)
+#define N_INDICES (1lu << 10)
+#define N_VALUES  (1lu << 31)
 
 int main(void) {
 
   shmem_init();
 
-  uint64_t *table = shmem_malloc(N_INDICES * sizeof(uint64_t));
+  uint64_t *table = shmem_calloc(N_INDICES, sizeof(uint64_t));
 
   int mype = shmem_my_pe();
   int npes = shmem_n_pes();
+  srand(mype);
 
-  shmem_bundle_start();
+  shmem_session_start(SHMEM_CTX_DEFAULT, SHMEM_SESSION_UNIFORM_AMO);
+
   for (size_t i = 0; i < N_UPDATES; i++) {
       int random_pe = rand() % npes;
       size_t random_idx = rand() % N_INDICES;
       uint64_t random_val = rand() % N_VALUES;
       shmem_uint64_atomic_xor(&table[random_idx], random_val, random_pe);
   }
-  shmem_bundle_stop();
+
+  shmem_session_stop(SHMEM_CTX_DEFAULT);
 
   shmem_free(table);
   shmem_finalize();
